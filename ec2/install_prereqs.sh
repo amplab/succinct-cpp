dcd0f6a8c0f95adb2863e4c086f678d7650e04cc
Anurag Khandelwal <anuragk@berkeley.edu>
2015-01-19 23:33:18 +0000

    Fixed pre-reqs

--- ec2/install_prereqs.sh
+++ ec2/install_prereqs.sh
@@ -5,6 +5,7 @@ yum -y install python-devel
 yum -y install openssl-devel
 yum -y install mysql-devel ruby-devel rubygems
 yum -y install boost-devel
+yum -y install flex
 
 mkdir -p /local/devtools
 
@@ -24,6 +25,15 @@ cd /local/devtools
 curl -OL http://ftpmirror.gnu.org/automake/automake-1.14.tar.gz
 tar xzf automake-1.14.tar.gz
 cd automake-1.14
+./configure --prefix=/usr/local
+make
+sudo make install
+
+# Install bison
+cd /local/devtools
+wget http://ftp.gnu.org/gnu/bison/bison-2.5.1.tar.gz
+tar xvf bison-2.5.1.tar.gz
+cd bison-2.5.1
 ./configure --prefix=/usr/local
 make
 sudo make install
