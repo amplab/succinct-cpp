185fb23e9e68b1213a331896338d71a8eda4b9cd
Anurag Khandelwal <anuragk@berkeley.edu>
2015-01-19 23:20:24 +0000

    Added ec2 script

---
+++ ec2/install_prereqs.sh
@@ -0,0 +1,29 @@
+# install prereqs
+yum -y install libevent
+yum -y install libevent-devel
+yum -y install python-devel
+yum -y install openssl-devel
+yum -y install mysql-devel ruby-devel rubygems
+yum -y install boost-devel
+
+mkdir -p /local/devtools
+
+# Install Autoconf
+yum -y install m4
+cd /local/devtools
+curl -OL http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz
+tar xzf autoconf-2.69.tar.gz
+cd autoconf-2.69
+./configure --prefix=/usr/local
+make
+sudo make install
+export PATH=/usr/local/bin:$PATH
+
+# Install Automake
+cd /local/devtools
+curl -OL http://ftpmirror.gnu.org/automake/automake-1.14.tar.gz
+tar xzf automake-1.14.tar.gz
+cd automake-1.14
+./configure --prefix=/usr/local
+make
+sudo make install
