b06529057ea9d99b793cebbf17e64b3ce406a8a4
Anurag Khandelwal <anuragk@berkeley.edu>
2015-02-16 18:43:57 -0800

    Added required functions for search and count to SuccinctShard

--- src/succinct/SuccinctShard.cpp
+++ src/succinct/SuccinctShard.cpp
@@ -25,6 +25,112 @@ SuccinctShard::SuccinctShard(uint32_t id, std::string filename, bool construct, 
     init_bitmap(&invalid_offsets, keys.size(), s_allocator);
 }
 
+uint64_t SuccinctShard::compute_context_value(const char *p, uint64_t i) {
+    uint64_t val = 0;
+    uint64_t max = MIN((i + npa->get_context_len()), input_size);
+    for (uint64_t t = i; t < max; t++) {
+        val = val * alphabet_size + alphabet_map[p[t]].second;
+    }
+
+    if (max < i + npa->get_context_len()) {
+        for (uint64_t t = 0; t < (i + npa->get_context_len()) % input_size; t++) {
+            val = val * alphabet_size + alphabet_map[p[t]].second;
+        }
+    }
+
+    return val;
+}
+
+std::pair<int64_t, int64_t> SuccinctShard::get_range_slow(const char *p,
+                                                        uint64_t len) {
+    std::pair<int64_t, int64_t> range(0, -1);
+    uint64_t m = strlen(p);
+    int64_t sp, ep, c1, c2;
+
+    if (alphabet_map.find(p[m - 1]) != alphabet_map.end()) {
+        sp = (alphabet_map[p[m - 1]]).first;
+        ep = alphabet_map[alphabet[alphabet_map[p[m - 1]].second + 1]].first - 1;
+    } else return range;
+
+    for (int64_t i = m - 2; i >= 0; i--) {
+        if (alphabet_map.find(p[m - 1]) != alphabet_map.end()) {
+            c1 = alphabet_map[p[i]].first;
+            c2 = alphabet_map[alphabet[alphabet_map[p[i]].second + 1]].first - 1;
+        } else return range;
+
+        if(c2 < c1) return range;
+
+        sp = npa->binary_search_npa(sp, c1, c2, false);
+        ep = npa->binary_search_npa(ep, c1, c2, true);
+
+        if (sp > ep) return range;
+    }
+
+    range.first = sp;
+    range.second = ep;
+
+    return range;
+}
+
+std::pair<int64_t, int64_t> SuccinctShard::get_range(const char *p,
+                                                    uint64_t len) {
+    uint64_t m = strlen(p);
+    if (m <= npa->get_context_len()) {
+       return get_range_slow(p, len);
+    }
+    std::pair<int64_t, int64_t> range(0, -1);
+    uint32_t sigma_id;
+    int64_t sp, ep, c1, c2;
+    uint64_t start_off;
+    uint64_t context_val, context_id;
+
+    sigma_id = alphabet_map[p[m - npa->get_context_len() - 1]].second;
+    context_val = compute_context_value(p, m - npa->get_context_len());
+    context_id = npa->contexts[context_val];
+    start_off = get_rank1(&(npa->col_nec[sigma_id]), context_id) - 1;
+    sp = npa->col_offsets[sigma_id] + npa->cell_offsets[sigma_id][start_off];
+    if(start_off + 1 < npa->cell_offsets[sigma_id].size()) {
+       ep = npa->col_offsets[sigma_id] + npa->cell_offsets[sigma_id][start_off + 1] - 1;
+    } else if((sigma_id + 1) < alphabet_size){
+       ep = npa->col_offsets[sigma_id + 1] - 1;
+    } else {
+       ep = input_size - 1;
+    }
+
+    if(sp > ep) return range;
+
+    for (int64_t i = m - npa->get_context_len() - 2; i >= 0; i--) {
+    	if (alphabet_map.find(p[i]) != alphabet_map.end()) {
+           sigma_id = alphabet_map[p[i]].second;
+           context_val = compute_context_value(p, i + 1);
+           if(npa->contexts.find(context_val) == npa->contexts.end()) {
+        	   return range;
+           }
+           context_id = npa->contexts[context_val];
+           start_off = get_rank1(&(npa->col_nec[sigma_id]), context_id) - 1;
+           c1 = npa->col_offsets[sigma_id] + npa->cell_offsets[sigma_id][start_off];
+           if(start_off + 1 < npa->cell_offsets[sigma_id].size()) {
+               c2 = npa->col_offsets[sigma_id] + npa->cell_offsets[sigma_id][start_off + 1] - 1;
+           } else if((sigma_id + 1) < alphabet_size){
+               c2 = npa->col_offsets[sigma_id + 1] - 1;
+           } else {
+               c2 = input_size - 1;
+           }
+           if(c2 < c1) return range;
+    	} else return range;
+
+		sp = npa->binary_search_npa(sp, c1, c2, false);
+		ep = npa->binary_search_npa(ep, c1, c2, true);
+
+		if (sp > ep) return range;
+    }
+
+    range.first = sp;
+    range.second = ep;
+
+    return range;
+}
+
 std::string SuccinctShard::name() {
     return this->succinct_datafile;
 }
