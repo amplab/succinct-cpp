73441dbb01e353ba1fb34e1166fabe4e90bdc0a8
Anurag Khandelwal <anuragk@berkeley.edu>
2015-02-27 14:31:35 -0800

    Added succinct-benchmark

--- benchmark/src/shard-bench.cpp
+++ benchmark/src/shard-bench.cpp
@@ -6,28 +6,50 @@
 #include "ShardBenchmark.hpp"
 
 void print_usage(char *exec) {
-    fprintf(stderr, "Usage: %s [-m mode] [-i isa_sampling_rate] [-n npa_sampling_rate] [-t type] [-l len] [file]\n", exec);
+    fprintf(stderr, "Usage: %s [-m mode] [-s sa_sampling_rate] [-i isa_sampling_rate] [-x sampling_scheme] [-n npa_sampling_rate] [-t type] [-l len] [file]\n", exec);
+}
+
+SamplingScheme scheme_from_opt(int opt) {
+    switch(opt) {
+    case 0:
+        return SamplingScheme::FLAT_SAMPLE_BY_INDEX;
+    case 1:
+        return SamplingScheme::FLAT_SAMPLE_BY_VALUE;
+    case 2:
+        return SamplingScheme::LAYERED_SAMPLE_BY_INDEX;
+    default:
+        return SamplingScheme::FLAT_SAMPLE_BY_INDEX;
+    }
 }
 
 int main(int argc, char **argv) {
-    if(argc < 2 || argc > 12) {
+    if(argc < 2 || argc > 16) {
         print_usage(argv[0]);
         return -1;
     }
 
     int c;
     uint32_t mode = 0;
+    uint32_t sa_sampling_rate = 32;
     uint32_t isa_sampling_rate = 32;
     uint32_t npa_sampling_rate = 128;
     std::string type = "latency-get";
     int32_t len = 100;
-    while((c = getopt(argc, argv, "m:i:n:t:l:")) != -1) {
+    SamplingScheme scheme = SamplingScheme::FLAT_SAMPLE_BY_INDEX;
+
+    while((c = getopt(argc, argv, "m:s:i:x:n:t:l:")) != -1) {
         switch(c) {
         case 'm':
             mode = atoi(optarg);
             break;
+        case 's':
+            sa_sampling_rate = atoi(optarg);
+            break;
         case 'i':
             isa_sampling_rate = atoi(optarg);
+            break;
+        case 'x':
+            scheme = scheme_from_opt(atoi(optarg));
             break;
         case 'n':
             npa_sampling_rate = atoi(optarg);
@@ -40,10 +62,12 @@ int main(int argc, char **argv) {
             break;
         default:
             mode = 0;
+            sa_sampling_rate = 32;
             isa_sampling_rate = 32;
             npa_sampling_rate = 128;
             type = "latency-get";
             len = 100;
+            scheme = SamplingScheme::FLAT_SAMPLE_BY_INDEX;
         }
     }
 
@@ -53,20 +77,17 @@ int main(int argc, char **argv) {
     }
 
     std::string inputpath = std::string(argv[optind]);
-    std::ifstream input(inputpath);
-    uint32_t num_keys = std::count(std::istreambuf_iterator<char>(input),
-            std::istreambuf_iterator<char>(), '\n');
 
     SuccinctShard *fd;
     if(mode == 0) {
-        fd = new SuccinctShard(0, inputpath, num_keys, true, isa_sampling_rate, npa_sampling_rate);
+        fd = new SuccinctShard(0, inputpath, true, sa_sampling_rate, isa_sampling_rate, npa_sampling_rate, scheme, scheme);
 
         // Serialize and save to file
         std::ofstream s_out(inputpath + ".succinct");
         fd->serialize(s_out);
         s_out.close();
     } else if(mode == 1) {
-        fd = new SuccinctShard(0, inputpath, num_keys, false, isa_sampling_rate, npa_sampling_rate);
+        fd = new SuccinctShard(0, inputpath, false, sa_sampling_rate, isa_sampling_rate, npa_sampling_rate, scheme, scheme);
     } else {
         // Only modes 0, 1 supported for now
         assert(0);
