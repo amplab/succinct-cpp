881b8b0bd2ff9c2e9b0f48105962456fbe497f26
Anurag Khandelwal <anuragk@berkeley.edu>
2015-03-19 19:24:32 -0700

    Modified script to recognize ISA and SA sampling rates

--- sbin/start-aservers-local.sh
+++ sbin/start-aservers-local.sh
@@ -28,7 +28,15 @@ fi
 mkdir -p $SUCCINCT_LOG_PATH
 
 if [ "$QUERY_SERVER_PORT" = "" ]; then
-	QUERY_SERVER_PORT=11002
+	QUERY_SERVER_PORT="11002"
+fi
+
+if [ "$ISA_SAMPLING_RATE" = "" ]; then
+    ISA_SAMPLING_RATE="32"
+fi
+
+if [ "$SA_SAMPLING_RATE" = "" ]; then
+    SA_SAMPLING_RATE="32"
 fi
 
 LIMIT=$(($1 - 1))
@@ -36,5 +44,5 @@ LIMIT=$(($1 - 1))
 for i in `seq 0 $LIMIT`; do
 	PORT=$(($QUERY_SERVER_PORT + $i))
 	DATA_FILE="$SUCCINCT_DATA_PATH/data"
-	nohup "$bin/aserver" -m 1 -p $PORT $OPP $DATA_FILE 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
+    nohup "$bin/aserver" -m 1 -p $PORT -i $ISA_SAMPLING_RATE -s $SA_SAMPLING_RATE $OPP $DATA_FILE 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
 done
