d2719d8110073fa6911cc59e32028f92f289d6db
Anurag Khandelwal <anuragk@berkeley.edu>
2015-03-24 14:22:43 -0700

    Added search to aqs-bench

--- benchmark/src/aqs-bench.cpp
+++ benchmark/src/aqs-bench.cpp
@@ -9,7 +9,7 @@ void print_usage(char *exec) {
 }
 
 int main(int argc, char **argv) {
-    if(argc > 9) {
+    if(argc > 11) {
         print_usage(argv[0]);
         return -1;
     }
@@ -19,7 +19,8 @@ int main(int argc, char **argv) {
     double skew = 1.0;  // Pure uniform
     int32_t len = 100;
     uint32_t batch_size = 10;
-    while((c = getopt(argc, argv, "o:z:l:b:")) != -1) {
+    std::string querypath = "";
+    while((c = getopt(argc, argv, "o:z:l:b:q:")) != -1) {
         switch(c) {
         case 'o':
             outpath = std::string(optarg);
@@ -33,22 +34,27 @@ int main(int argc, char **argv) {
         case 'b':
             batch_size = atol(optarg);
             break;
+        case 'q':
+            querypath = optarg;
+            break;
         default:
             outpath = "benchmark/res";
             skew = 1.0;
             len = 100;
             batch_size = 10;
+            querypath = "";
         }
     }
 
     // Benchmark core functions
     std::string reqpath = outpath + "/aqs-bench.req";
     std::string respath = outpath + "/aqs-bench.res";
-    AdaptiveQueryServerBenchmark aqs_bench(reqpath, respath, skew, len);
+    AdaptiveQueryServerBenchmark aqs_bench(reqpath, respath, skew, len, querypath);
     for(int32_t i = -1; i < 10; i++) {
         aqs_bench.delete_layer(i);
-        aqs_bench.measure_throughput();
-        aqs_bench.measure_throughput_batch(batch_size);
+        aqs_bench.measure_throughput_search();
+        // aqs_bench.measure_throughput();
+        // aqs_bench.measure_throughput_batch(batch_size);
     }
 
     return 0;
