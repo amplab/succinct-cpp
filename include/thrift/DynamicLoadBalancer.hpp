d5e8c7fc460c61c79a038d8990d7f4fb006a1605
Anurag Khandelwal <anuragk@berkeley.edu>
2015-03-25 14:33:51 -0700

    Added get_replica based on inst. queue lengths

--- include/thrift/DynamicLoadBalancer.hpp
+++ include/thrift/DynamicLoadBalancer.hpp
@@ -36,6 +36,24 @@ public:
         return 0;
     }
 
+    uint32_t get_replica(std::atomic<uint64_t> *priorities) {
+        // Get the distribution
+        double sum = 0.0;
+        for(uint32_t i = 0; i < replication; i++) {
+            double prio = priorities[i];
+            sum += (1 / (prio + 1));  // To prevent zero priority cases
+            this->cum_prio[i] = sum;
+        }
+
+        double r = ((double) rand() / (RAND_MAX));
+        for(size_t i = 0; i < replication; i++) {
+            if(r < (cum_prio[i] / sum)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
     void get_latest_allocations(std::vector<double> &allocations) {
         double prev = 0.0;
         for(uint32_t i = 0; i < replication; i++) {
