90197ab4ba76ef08b235e5d45c03fcc0d6b73d99
Anurag Khandelwal <anuragk@berkeley.edu>
2015-03-25 10:21:53 -0700

    Fixed blowfish server script

--- sbin/start-aservers-local.sh
+++ sbin/start-aservers-local.sh
@@ -39,10 +39,22 @@ if [ "$SA_SAMPLING_RATE" = "" ]; then
     SA_SAMPLING_RATE="32"
 fi
 
+if [ "$OPPORTUNISTIC" = "TRUE" ]; then
+    OPP="-o"
+elif
+    OPP=""
+fi
+
+if [ "$STANDALONE" = "TRUE" ]; then
+    STD="-d"
+elif
+    STD=""
+fi
+
 LIMIT=$(($1 - 1))
 
 for i in `seq 0 $LIMIT`; do
 	PORT=$(($QUERY_SERVER_PORT + $i))
 	DATA_FILE="$SUCCINCT_DATA_PATH/data"
-    nohup "$bin/aserver" -m 1 -p $PORT -i $ISA_SAMPLING_RATE -s $SA_SAMPLING_RATE $OPP $DATA_FILE 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
+    nohup "$bin/aserver" -m 1 -p $PORT -i $ISA_SAMPLING_RATE -s $SA_SAMPLING_RATE $STD $OPP $DATA_FILE 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
 done
