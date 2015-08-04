6cad144203826ead2270b98857dd3b42f5d12684
Anurag Khandelwal <anuragk@berkeley.edu>
2015-08-04 13:25:16 -0700

    Fixed startup script

--- sbin/start-servers-local.sh
+++ sbin/start-servers-local.sh
@@ -36,25 +36,20 @@ if [ "$REGEX_OPT" = "" ]; then
 fi
 
 if [ "$REGEX_OPT" = "TRUE" ]; then
-    opt="-o"
+    OPT="-o"
 fi
 
-i=0
-while read sr dist; do
-	((sampling_rates[$i] = $sr))
-	i=$(($i + 1))
-done < ${SUCCINCT_CONF_DIR}/repl
+if [ "$SA_SAMPLING_RATE" = "" ]; then
+    SA_SAMPLING_RATE="32"
+fi
 
-num_shards=$1
-num_hosts=$2
-local_host_id=$3
-num_replicas=$( wc -l < ${SUCCINCT_CONF_DIR}/repl)
+if [ "$ISA_SAMPLING_RATE" = "TRUE" ]; then
+    ISA_SAMPLING_RATE="32"
+fi
+
 limit=$(($1 - 1))
-
 for i in `seq 0 $limit`; do
-	port=$(($QUERY_SERVER_PORT + $i))
-	shard_id=$(($i * $num_hosts + local_host_id))
-	shard_type=$(($shard_id % $num_replicas))
-	data_file="$SUCCINCT_DATA_PATH/data_${shard_type}"
-	nohup "$bin/sserver" -m 1 -p $port -i ${sampling_rates[$shard_type]} $opt $data_file 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
+	PORT=$(($QUERY_SERVER_PORT + $i))
+	DATA_FILE="$SUCCINCT_DATA_PATH/data_$i"
+	nohup "$bin/sserver" -m 1 -p $PORT -s $SA_SAMPLING_RATE -i $ISA_SAMPLING_RATE $OPT $DATA_FILE 2>"$SUCCINCT_LOG_PATH/server_${i}.log" &
 done
