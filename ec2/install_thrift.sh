c72aa1ee19b89c97a0e0a32f31e303b4c43974af
Anurag Khandelwal <anuragk@berkeley.edu>
2015-08-25 15:53:20 -0700

    Fixed install_thrift script

--- ec2/install_thrift.sh
+++ ec2/install_thrift.sh
@@ -1,15 +1,19 @@
-sudo yum -y update
+# Must run scripts logged in as root
+yum -y update
 
-sudo yum -y groupinstall "Development Tools"
+yum -y groupinstall "Development Tools"
 
-sudo yum install -y wget
+yum install -y wget
+
+mkdir tmpdir
+cd tmpdir
 
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar xvf autoconf-2.69.tar.gz
 cd autoconf-2.69
 ./configure --prefix=/usr
 make
-sudo make install
+make install
 cd ..
 
 wget http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz
@@ -17,7 +21,7 @@ tar xvf automake-1.14.tar.gz
 cd automake-1.14
 ./configure --prefix=/usr
 make
-sudo make install
+make install
 cd ..
 
 wget http://ftp.gnu.org/gnu/bison/bison-2.5.1.tar.gz
@@ -25,16 +29,16 @@ tar xvf bison-2.5.1.tar.gz
 cd bison-2.5.1
 ./configure --prefix=/usr
 make
-sudo make install
+make install
 cd ..
 
-sudo yum -y install libevent-devel zlib-devel openssl-devel
+yum -y install libevent-devel zlib-devel openssl-devel
 
 wget http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz
 tar xvf boost_1_55_0.tar.gz
 cd boost_1_55_0
 ./bootstrap.sh
-sudo ./b2 install
+./b2 install
 cd ..
 
 git clone https://git-wip-us.apache.org/repos/asf/thrift.git
@@ -42,5 +46,8 @@ cd thrift
 ./bootstrap.sh
 ./configure --with-lua=no
 make
-sudo make install
+make install
 cd ..
+
+cd ..
+rm -rf tmpdir
