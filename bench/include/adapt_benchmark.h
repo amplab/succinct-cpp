1b4b4146ed8395af5a9b2c21b2bb8f420021dcdd
Anurag Khandelwal <anuragk@berkeley.edu>
2015-09-17 15:14:43 -0700

    Updated parameters

--- bench/include/adapt_benchmark.h
+++ bench/include/adapt_benchmark.h
@@ -264,7 +264,7 @@ class AdaptBenchmark : public Benchmark {
     uint64_t query_count = query_client_->get_num_keys();
     fprintf(stderr, "Generating zipf distribution with theta=%f, N=%llu...\n",
             skew_, query_count);
-    ZipfGenerator z(skew_, 100000);
+    ZipfGenerator z(skew_, 1000000);
     fprintf(stderr, "Generated zipf distribution, generating keys...\n");
     for (uint64_t i = 0; i < query_count; i++) {
       randoms_.push_back(z.Next());
@@ -290,8 +290,8 @@ class AdaptBenchmark : public Benchmark {
     }
     inputfile.close();
     fprintf(stderr, "Generating zipf distribution with theta=%f, N=%zu...\n",
-                skew_, 1000000);
-    ZipfGenerator z(skew_, 1000000);
+                skew_, 100000);
+    ZipfGenerator z(skew_, 100000);
     fprintf(stderr, "Generated zipf distribution, generating query ids...\n");
     for (uint64_t i = 0; i < queries_.size(); i++) {
       query_ids_.push_back(z.Next());
