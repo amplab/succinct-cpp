7463c23623ee5751bb4e3f1f52fdc69e25b3f9d0
Anurag Khandelwal <anuragk@berkeley.edu>
2016-01-21 14:19:02 -0800

    Updated succinct-env configuration parameters

--- conf/succinct-env.sh
+++ conf/succinct-env.sh
@@ -1,7 +1,17 @@
-export NUM_SHARDS="8"
+export SHARDS_PER_SERVER="8"
+
+export MASTER_LOG_LEVEL="2" # Info
+export HANDLER_LOG_LEVEL="2" # Info
+export SERVER_LOG_LEVEL="2" # Info
+
+export MASTER_PORT="11001"
+export HANDLER_PORT="11002"
+export SERVER_PORT="11003"
+
+## Core parameters
 # export ISA_SAMPLING_RATE="32"
 # export SA_SAMPLING_RATE="32"
-# export NPA_SAMPLING_RATE="32"
-# export SA_SAMPLING_SCHEME="32"
-# export ISA_SAMPLING_SCHEME="32"
-# export NPA_SAMPLING_SCHEME="32"
+# export NPA_SAMPLING_RATE="128"
+# export SA_SAMPLING_SCHEME="0"
+# export ISA_SAMPLING_SCHEME="0"
+# export NPA_SAMPLING_SCHEME="1"
