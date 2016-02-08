5d23633adae2c131dab63b9998eb9acca1d517c3
Anurag Khandelwal <anuragk@berkeley.edu>
2016-02-07 22:58:32 -0800

    [TEMP] Disable heartbeats

--- handler/src/handler_impl.cc
+++ handler/src/handler_impl.cc
@@ -77,7 +77,7 @@ class HandlerImpl : virtual public HandlerIf {
 
     logger_.Info("Initialization successful.");
 
-    hb_manager_.Start();
+    // hb_manager_.Start();
 
     return 0;
   }
@@ -401,7 +401,7 @@ int main(int argc, char **argv) {
   Logger logger(static_cast<Logger::Level>(conf.GetInt("HANDLER_LOG_LEVEL")),
                 desc);
 
-  // succinct::HandlerHeartbeatManager hb_manager(host_id, conf, logger);
+  succinct::HandlerHeartbeatManager hb_manager(host_id, conf, logger);
 
   int port = conf.GetInt("HANDLER_PORT");
   try {
