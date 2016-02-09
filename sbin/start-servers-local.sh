a8f6553d004580caed6bd7b704ee8d7850bae8d0
Anurag Khandelwal <anuragk@berkeley.edu>
2016-02-08 16:49:45 -0800

    Updated scripts to assign unique ids to servers

--- sbin/start-servers-local.sh
+++ sbin/start-servers-local.sh
@@ -29,5 +29,6 @@ fi
 limit=$(($1 - 1))
 for i in `seq 0 $limit`; do
 	DATA_FILE="$SUCCINCT_DATA_PATH/data_$i"
+  SHARD_ID=$(($1 * $2 + $i))
 	nohup "$bin/sserver" $i $DATA_FILE >/dev/null 2>"$LOG_PATH/stderr/server_$i.stderr" &
 done
--- sbin/start-servers.sh
+++ sbin/start-servers.sh
@@ -36,15 +36,17 @@ if [ "$SUCCINCT_SSH_OPTS" = "" ]; then
   SUCCINCT_SSH_OPTS="-o StrictHostKeyChecking=no"
 fi
 
+i=0
 for host in `echo "$HOSTLIST"|sed  "s/#.*$//;/^$/d"`; do
   if [ -n "${SUCCINCT_SSH_FOREGROUND}" ]; then
-    ssh $SUCCINCT_SSH_OPTS "$host" "$sbin/start-servers-local.sh" $SHARDS_PER_SERVER \
+    ssh $SUCCINCT_SSH_OPTS "$host" "$sbin/start-servers-local.sh" $SHARDS_PER_SERVER $i\
       2>&1 | sed "s/^/$host: /"
   else
-    ssh $SUCCINCT_SSH_OPTS "$host" "$sbin/start-servers-local.sh" $SHARDS_PER_SERVER \
+    ssh $SUCCINCT_SSH_OPTS "$host" "$sbin/start-servers-local.sh" $SHARDS_PER_SERVER $i\
       2>&1 | sed "s/^/$host: /" &
   fi
   if [ "$SUCCINCT_HOST_SLEEP" != "" ]; then
     sleep $SUCCINCT_HOST_SLEEP
   fi
+  i=$((i+1))
 done
