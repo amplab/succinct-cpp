424c50f7f659db0ffd43f9398df5c2f6ccbabc44
Anurag Khandelwal <anuragk@berkeley.edu>
2016-04-20 19:15:10 -0700

    Updated ec2 setup script

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -4,8 +4,10 @@ curdir="`dirname "$0"`"
 curdir="`cd "$curdir"; pwd`"
 
 # Install dependencies on coordinator and all servers
+$curdir/../sbin/sync $curdir/../
+
 $curdir/install-dependencies.sh &
-$curdir/../sbin/hosts.sh $curdir/install-dependencies.sh
+$curdir/../sbin/servers.sh $curdir/install-dependencies.sh
 wait
 
 echo "Finished installing dependencies on coordinator and all servers"
