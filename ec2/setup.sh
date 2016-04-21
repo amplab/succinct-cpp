c60e49e76cf98d8fdaa338a3deaa1229cd7cb6cf
Anurag Khandelwal <anuragk@berkeley.edu>
2016-04-21 13:45:48 -0700

    Updated setup script

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -3,7 +3,7 @@
 curdir="`dirname "$0"`"
 curdir="`cd "$curdir"; pwd`"
 
-# Install dependencies on coordinator and all servers
+# Install dependencies and succinct on coordinator and all servers
 $curdir/../sbin/sync $curdir/../
 
 $curdir/install-dependencies.sh &
@@ -11,3 +11,9 @@ $curdir/../sbin/servers.sh $curdir/install-dependencies.sh
 wait
 
 echo "Finished installing dependencies on coordinator and all servers"
+
+cd $curdir/../
+./build.sh
+
+# FIXME: Assumes the architecture on both coordinator and servers is the same
+./sbin/sync .
