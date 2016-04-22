ab8e00a58564c5a2c7082c95ab5c2a4124848a18
Anurag Khandelwal <anuragk@berkeley.edu>
2016-04-22 14:39:20 -0700

    Updated default succinct-env parameters on ec2

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -15,5 +15,9 @@ echo "Finished installing dependencies on coordinator and all servers"
 cd $curdir/../
 ./build.sh
 
+cp $curdir/succinct-env.sh.template conf/succinct-env.sh
+coordinator=`curl -s http://169.254.169.254/latest/meta-data/public-hostname`
+echo "export COORDINATOR_HOSTNAME=\"$coordinator\"" >> conf/succinct-env.sh
+
 # FIXME: Assumes the architecture on both coordinator and servers is the same
 ./sbin/sync .
