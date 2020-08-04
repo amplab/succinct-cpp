02b86f8f432d94cb054d6b4db4e6ec9d95f314fc
Daniel Li <danielli@MacBook-Pro-3.local>
2020-08-03 21:15:06 -0400

    pipelined threads are now added via indices instead of append

--- benchmark-testing/chunk_compressor.py
+++ benchmark-testing/chunk_compressor.py
@@ -71,108 +71,110 @@ class ReusableThread(Thread):
 def do_nothing(*args):
     pass
 
-# **** PARALLEL EXECUTION WITH REUSABLE THREADS ****
-def read_chunk(read_index, chunk_string, read_arg, s3, code_start):
-    # Starting to read chunk
-    global read_time
-    i = read_index[0]
-    print("reading chunk " + str(i))
-    start = time.time()
-
-    ###
-    obj = s3.get_object(Bucket='succinct-datasets', Key=chunk_string + str(i) + ".succinct")
-    read_arg[0] = obj['Body'].read().decode('utf-8')
-    del obj
-    ###
-
-    # write chunk times to file and mark time
-    # end = time.time()
-    # read_time += end - start
-    # read_time_file = open("read_time_file.txt","a")
-    # read_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
-
-def compress_chunk(compress_index, read_arg, compressed_arg, code_start):
-    # Starting to compress chunk
-    global compress_time
-    i = compress_index[0]
-    print("compressing chunk " + str(i))
-    start = time.time()
-
-    ###
-    q = file.File(0, read_arg[0], 32, 32, 128, 0, 1)
-    compressed_arg[0] = q.GetContent().tobytes()
-    q.DeleteContent()
-    del q
-    ###
-
-    # write chunk times to file and mark time
-    # end = time.time()
-    # compress_time += end - start
-    # compress_time_file = open("compress_time_file.txt","a")
-    # compress_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
-
-def upload_chunk(upload_index, chunk_string, compressed_arg, s3, code_start):
-    # Starting to upload chunk
-    global upload_time
-    i = upload_index[0]
-    print("uploading chunk " + str(i))
-    start = time.time()
-
-    ###
-    s3.put_object(Body=compressed_arg[0], Bucket='succinct-datasets', Key=chunk_string + "compressed-" + str(i) + ".succinct")
-    ###
-
-    # write chunk times to file and mark time
-    # end = time.time()
-    # upload_time += end - start
-    # upload_time_file = open("upload_time_file.txt","a")
-    # upload_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
-
-
-# # **** PARALLEL EXECUTION WITH LIST OF THREAD FUNCTIONS ****
-
-# def read_chunk(i, chunk_string, read_chunks, s3, code_start):
-#     print("reading chunk " + str(i))
+# # **** PARALLEL EXECUTION WITH REUSABLE THREADS ****
+# def read_chunk(read_index, chunk_string, read_arg, s3, code_start):
+#     # Starting to read chunk
 #     global read_time
+#     i = read_index[0]
+#     print("reading chunk " + str(i))
 #     start = time.time()
+
+#     ###
 #     obj = s3.get_object(Bucket='succinct-datasets', Key=chunk_string + str(i) + ".succinct")
-#     read_chunks.append(obj['Body'].read().decode('utf-8'))
+#     read_arg[0] = obj['Body'].read().decode('utf-8')
 #     del obj
-#     end = time.time()
-#     read_time += end - start
+#     ###
 
-#     # # write chunk times to file
+#     # write chunk times to file and mark time
+#     # end = time.time()
+#     # read_time += end - start
 #     # read_time_file = open("read_time_file.txt","a")
-#     # read_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " finish: " + str(end - code_start) + "\n")
+#     # read_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
 
-# def compress_chunk(i, read_chunks, compressed_chunks, code_start):
-#     print("compressing chunk " + str(i))
+# def compress_chunk(compress_index, read_arg, compressed_arg, code_start):
+#     # Starting to compress chunk
 #     global compress_time
+#     i = compress_index[0]
+#     print("compressing chunk " + str(i))
 #     start = time.time()
-#     q = file.File(0, read_chunks[i], 32, 32, 128, 0, 1)
-#     compressed_chunks.append(q.GetContent().tobytes())
+
+#     ###
+#     q = file.File(0, read_arg[0], 32, 32, 128, 0, 1)
+#     compressed_arg[0] = q.GetContent().tobytes()
 #     q.DeleteContent()
 #     del q
-#     read_chunks[i] = None
-#     end = time.time()
-#     compress_time += end - start
+#     ###
 
-#     # # write chunk times to file
+#     # write chunk times to file and mark time
+#     # end = time.time()
+#     # compress_time += end - start
 #     # compress_time_file = open("compress_time_file.txt","a")
-#     # compress_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " finish: " + str(end - code_start) + "\n")
+#     # compress_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
 
-# def upload_chunk(i, chunk_string, compressed_chunks, s3, code_start):
-#     print("uploading chunk " + str(i))
+# def upload_chunk(upload_index, chunk_string, compressed_arg, s3, code_start):
+#     # Starting to upload chunk
 #     global upload_time
+#     i = upload_index[0]
+#     print("uploading chunk " + str(i))
 #     start = time.time()
-#     s3.put_object(Body=compressed_chunks[i], Bucket='succinct-datasets', Key=chunk_string + "compressed-" + str(i) + ".succinct")
-#     compressed_chunks[i] = None
-#     end = time.time()
-#     upload_time += end - start
 
-    # # write chunk times to file
-    # upload_time_file = open("upload_time_file.txt","a")
-    # upload_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " finish: " + str(end - code_start) + "\n")
+#     ###
+#     s3.put_object(Body=compressed_arg[0], Bucket='succinct-datasets', Key=chunk_string + "compressed-" + str(i) + ".succinct")
+#     ###
+
+#     # write chunk times to file and mark time
+#     # end = time.time()
+#     # upload_time += end - start
+#     # upload_time_file = open("upload_time_file.txt","a")
+#     # upload_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
+
+
+# **** PARALLEL EXECUTION WITH LIST OF THREAD FUNCTIONS ****
+
+def read_chunk(i, chunk_string, read_chunks, s3, code_start):
+    print("reading chunk " + str(i))
+    global read_time
+    start = time.time()
+    obj = s3.get_object(Bucket='succinct-datasets', Key=chunk_string + str(i) + ".succinct")
+    # writing read chunk str(i) to pos len(read_chunks)
+    read_chunks[i] = obj['Body'].read().decode('utf-8')
+    del obj
+    end = time.time()
+    read_time += end - start
+
+    # write chunk times to file
+    read_time_file = open("read_time_file.txt","a")
+    read_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
+
+def compress_chunk(i, read_chunks, compressed_chunks, code_start):
+    print("compressing chunk " + str(i))
+    global compress_time
+    start = time.time()
+    q = file.File(0, read_chunks[i], 32, 32, 128, 0, 1)
+     # writing compressed chunk str(i) to pos len(compressed_chunks)
+    compressed_chunks[i] = q.GetContent().tobytes()
+    q.DeleteContent()
+    del q
+    read_chunks[i] = None
+    end = time.time()
+    compress_time += end - start
+
+    # write chunk times to file
+    compress_time_file = open("compress_time_file.txt","a")
+    compress_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
+
+def upload_chunk(i, chunk_string, compressed_chunks, s3, code_start):
+    print("uploading chunk " + str(i))
+    global upload_time
+    start = time.time()
+    s3.put_object(Body=compressed_chunks[i], Bucket='succinct-datasets', Key=chunk_string + "compressed-" + str(i) + ".succinct")
+    compressed_chunks[i] = None
+    end = time.time()
+    upload_time += end - start
+
+    # write chunk times to file
+    upload_time_file = open("upload_time_file.txt","a")
+    upload_time_file.write("chunk " + str(i) +  " start: " + str(start - code_start) + " duration: " + str(end - start) + "\n")
 
 # **** FUNCTION START ****
 # compress file
@@ -181,8 +183,8 @@ start = time.time()
 chunk_string = "sample.txt" + "-chunk-"
 # Depends on number of chunks file is split into
 num_chunks = 1167
-read_chunks = []
-compressed_chunks = []
+read_chunks = [None] * num_chunks
+compressed_chunks = [None] * num_chunks
 
 # COMPRESS WITH FILES
 # q = file.File("sample.txt-chunk-1160.succinct", 32, 32, 128, 0, 1)
@@ -203,161 +205,164 @@ compressed_chunks = []
 
 # signal.signal(signal.SIGUSR1, do_nothing)
 
-# **** PARALLEL EXECUTION WITH REUSABLE THREADS ****
+# # **** PARALLEL EXECUTION WITH REUSABLE THREADS ****
 
-read_index = [0]
-compress_index = [0]
-upload_index = [0]
+# read_index = [0]
+# compress_index = [0]
+# upload_index = [0]
 
 
-# Variables to keep track of read and compressed data
-read_arg = [0]
-compressed_arg = [0]
+# # Variables to keep track of read and compressed data
+# read_arg = [0]
+# compressed_arg = [0]
 
-read_thread = ReusableThread(target = read_chunk, args=(read_index, chunk_string, read_arg, s3, start))
-compress_thread = ReusableThread(target = compress_chunk, args=(compress_index, read_arg, compressed_arg, start))
-upload_thread = ReusableThread(target = upload_chunk, args=(upload_index, chunk_string, compressed_arg, s3, start))
-
-for i in range(0, num_chunks + 2):
-    if (i == 0):
-        # START: read
-        read_index[0] = i
-
-        read_thread.start()
-    elif (i == 1):
-        # RESTART: read START: compress
-        read_thread.join()
-
-        read_index[0] = i
-        compress_index[0] = i - 1
-
-        compress_thread.start()
-        read_thread.restart()
-    elif (i == 2):
-        # RESTART: compress, read START: upload
-        read_thread.join()
-        compress_thread.join()
-
-        read_index[0] = i
-        compress_index[0] = i - 1
-        upload_index[0] = i - 2
-
-        upload_thread.start()
-        compress_thread.restart()
-        read_thread.restart()
-
-    elif (i == num_chunks):
-        # RESTART: upload, compress
-        read_thread.join()
-        compress_thread.join()
-        upload_thread.join()
-
-        compress_index [0] = i - 1
-        upload_index [0] = i - 2
-
-        upload_thread.restart()
-        compress_thread.restart()
-    elif (i == num_chunks + 1):
-        # RESTART: upload
-        compress_thread.join()
-        upload_thread.join()
-
-        upload_index [0] = i - 2
-
-        upload_thread.restart()
-
-        # Join last thread and finish all threads
-        upload_thread.join()
-
-        read_thread.finish()
-        compress_thread.finish()
-        upload_thread.finish()
-    else:
-        # RESTART: upload, compress, read
-        read_thread.join()
-        compress_thread.join()
-        upload_thread.join()
-
-        read_index[0] = i
-        compress_index[0] = i - 1
-        upload_index[0] = i - 2
-
-        upload_thread.restart()
-        compress_thread.restart()
-        read_thread.restart()
-
-# # **** PARALLEL EXECUTION WITH LIST OF THREADS ****
-# read_threads = []
-# compress_threads = []
-# upload_threads = []
+# read_thread = ReusableThread(target = read_chunk, args=(read_index, chunk_string, read_arg, s3, start))
+# compress_thread = ReusableThread(target = compress_chunk, args=(compress_index, read_arg, compressed_arg, start))
+# upload_thread = ReusableThread(target = upload_chunk, args=(upload_index, chunk_string, compressed_arg, s3, start))
 
 # for i in range(0, num_chunks + 2):
 #     if (i == 0):
-#         # Create read thread
-#         read_thread = Thread(target=read_chunk, args=(0, chunk_string, read_chunks, s3, start))
+#         # START: read
+#         read_index[0] = i
+
 #         read_thread.start()
-#         read_threads.append(read_thread)
 #     elif (i == 1):
-#          # Create read thread
-#         read_thread = Thread(target=read_chunk, args=(1, chunk_string, read_chunks, s3, start))
-#         read_thread.start()
-#         read_threads.append(read_thread)
+#         # RESTART: read START: compress
+#         read_thread.join()
 
-#         # If read_threads[0] isn't done, finish
-#         if read_threads[0].is_alive():
-#             read_threads[0].join()
-#         # Create compress thread
-#         compress_thread = Thread(target=compress_chunk, args=(0, read_chunks, compressed_chunks, start))
+#         read_index[0] = i
+#         compress_index[0] = i - 1
+
 #         compress_thread.start()
-#         compress_threads.append(compress_thread)
+#         read_thread.restart()
+#     elif (i == 2):
+#         # RESTART: compress, read START: upload
+#         read_thread.join()
+#         compress_thread.join()
+
+#         read_index[0] = i
+#         compress_index[0] = i - 1
+#         upload_index[0] = i - 2
+
+#         upload_thread.start()
+#         compress_thread.restart()
+#         read_thread.restart()
+
 #     elif (i == num_chunks):
-#         # If read_threads[num_chunks - 1] isn't done, finish
-#         if read_threads[i - 1].is_alive():
-#             read_threads[i - 1].join()
+#         # RESTART: upload, compress
+#         read_thread.join()
+#         compress_thread.join()
+#         upload_thread.join()
 
-#         # If compress_threads[num_chunks - 2] isn't done, finish
-#         if compress_threads[i - 2].is_alive():
-#             compress_threads[i - 2].join()
-        
-#         # Create compress thread
-#         compress_thread = Thread(target=compress_chunk, args=(i - 1, read_chunks, compressed_chunks, start))
-#         compress_thread.start()
-#         compress_threads.append(compress_thread)
-#         # Create upload thread
-#         upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
-#         upload_thread.start()
-#         upload_threads.append(upload_thread)
+#         compress_index [0] = i - 1
+#         upload_index [0] = i - 2
+
+#         upload_thread.restart()
+#         compress_thread.restart()
 #     elif (i == num_chunks + 1):
-#         # If compress_threads[num_chunks - 1] isn't done, finish
-#         if compress_threads[i - 2].is_alive():
-#             compress_threads[i - 2].join()
-#         # Create upload thread
-#         upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
-#         upload_thread.start()
-#         upload_threads.append(upload_thread)
+#         # RESTART: upload
+#         compress_thread.join()
+#         upload_thread.join()
+
+#         upload_index [0] = i - 2
+
+#         upload_thread.restart()
+
+#         # Join last thread and finish all threads
+#         upload_thread.join()
+
+#         read_thread.finish()
+#         compress_thread.finish()
+#         upload_thread.finish()
 #     else:
-#         # Create read thread
-#         read_thread = Thread(target=read_chunk, args=(i, chunk_string, read_chunks, s3, start))
-#         read_thread.start()
-#         read_threads.append(read_thread)
+#         # RESTART: upload, compress, read
+#         read_thread.join()
+#         compress_thread.join()
+#         upload_thread.join()
 
-#         # If read_threads[i - 1] isn't done, finish
-#         if read_threads[i - 1].is_alive():
-#             read_threads[i - 1].join()
+#         read_index[0] = i
+#         compress_index[0] = i - 1
+#         upload_index[0] = i - 2
+
+#         upload_thread.restart()
+#         compress_thread.restart()
+#         read_thread.restart()
+
+# **** PARALLEL EXECUTION WITH LIST OF THREADS ****
+read_threads = [None] * num_chunks
+compress_threads = [None] * num_chunks
+upload_threads = [None] * num_chunks
+
+for i in range(0, num_chunks + 2):
+    if (i == 0):
+        # Create read thread
+        read_thread = Thread(target=read_chunk, args=(0, chunk_string, read_chunks, s3, start))
+        read_thread.start()
+        read_threads[i] = read_thread
+
+    elif (i == 1):
+        # If read_threads[0] isn't done, finish
+        if read_threads[0].is_alive():
+            read_threads[0].join()
+
+        # Create read thread
+        read_thread = Thread(target=read_chunk, args=(1, chunk_string, read_chunks, s3, start))
+        read_thread.start()
+        read_threads[i] = read_thread
+
+        # Create compress thread
+        compress_thread = Thread(target=compress_chunk, args=(0, read_chunks, compressed_chunks, start))
+        compress_thread.start()
+        compress_threads[i-1] = compress_thread
+    elif (i == num_chunks):
+        # If read_threads[num_chunks - 1] isn't done, finish
+        if read_threads[i - 1].is_alive():
+            read_threads[i - 1].join()
+
+        # If compress_threads[num_chunks - 2] isn't done, finish
+        if compress_threads[i - 2].is_alive():
+            compress_threads[i - 2].join()
+        
+        # Create compress thread
+        compress_thread = Thread(target=compress_chunk, args=(i - 1, read_chunks, compressed_chunks, start))
+        compress_thread.start()
+        compress_threads[i-1] = compress_thread
+
+        # Create upload thread
+        upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
+        upload_thread.start()
+        upload_threads[i-2] = upload_thread
+    elif (i == num_chunks + 1):
+        # If compress_threads[num_chunks - 1] isn't done, finish
+        if compress_threads[i - 2].is_alive():
+            compress_threads[i - 2].join()
+        # Create upload thread
+        upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
+        upload_thread.start()
+        upload_threads[i-2] = upload_thread
+    else:
+        # If read_threads[i - 1] isn't done, finish
+        if read_threads[i - 1].is_alive():
+            read_threads[i - 1].join()
             
-#         # If compress_threads[i - 2] isn't done, finish
-#         if compress_threads[i - 2].is_alive():
-#             compress_threads[i - 2].join()
+        # If compress_threads[i - 2] isn't done, finish
+        if compress_threads[i - 2].is_alive():
+            compress_threads[i - 2].join()
 
-#         # Create compress thread
-#         compress_thread = Thread(target=compress_chunk, args=(i - 1, read_chunks, compressed_chunks, start))
-#         compress_thread.start()
-#         compress_threads.append(compress_thread)
+        # Create read thread
+        read_thread = Thread(target=read_chunk, args=(i, chunk_string, read_chunks, s3, start))
+        read_thread.start()
+        read_threads[i] = read_thread
 
-#         # Create upload thread
-#         upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
-#         upload_thread.start()
-#         upload_threads.append(upload_thread)
+        # Create compress thread
+        compress_thread = Thread(target=compress_chunk, args=(i - 1, read_chunks, compressed_chunks, start))
+        compress_thread.start()
+        compress_threads[i-1] = compress_thread
+
+        # Create upload thread
+        upload_thread = Thread(target=upload_chunk, args=(i - 2, chunk_string, compressed_chunks, s3, start))
+        upload_thread.start()
+        upload_threads[i-2] = upload_thread
 
 # os.kill(PID, signal.SIGUSR1)
 
